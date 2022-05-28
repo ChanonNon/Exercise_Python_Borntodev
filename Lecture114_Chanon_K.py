4e103beb9302bad5dc39d8b64e1b0d26a239283d
ChanonNon <nonchak42@gmail.com>
2022-05-28 12:50:08 +0700

    Update Lecture114_Chanon_K.py

--- Lecture114_Chanon_K.py
+++ Lecture114_Chanon_K.py
@@ -1,4 +1,30 @@
-import tkinter as tk
-app = tk.Tk()
-app.title('Hello world')
+import datetime as dt
+from decimal import *
+from forex_python.converter import CurrencyRates
+
+from tkinter import *
+
+def button_clik(event):
+    c = CurrencyRates()
+    # print(type(textBoxHight.get()))
+    result4 = c.convert(textBoxHight.get(), 'THB',float(textBoxWeight.get())) 
+    print(f"aaaa {'%.2f'%(result4)}")
+
+app = Tk()
+app.title('Forex')
+
+labelHight = Label(app,text = "เลือกสกุลเงิน : ")
+labelHight.grid(row = 0,column = 0)
+textBoxHight = Entry(app)
+textBoxHight.grid(row=0,column=1)
+
+labelWeight = Label(app,text = "จำนวนเงิน : ")
+labelWeight.grid(row = 1,column = 0)
+textBoxWeight = Entry(app)
+textBoxWeight.grid(row=1,column=1)
+
+calculableButton = Button(app,text = "คำนวณ")
+calculableButton.bind("<Button-1>",button_clik)
+calculableButton.grid(row=2,column=0)
+
 app.mainloop()
