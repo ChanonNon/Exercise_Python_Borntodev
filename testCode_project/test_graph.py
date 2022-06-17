77e5d89e6b152a156f16f10df3547fc4b1397c62
ChanonNon <nonchak42@gmail.com>
2022-06-17 13:41:54 +0700

    Update test_graph.py

--- testCode_project/test_graph.py
+++ testCode_project/test_graph.py
@@ -1,62 +1,129 @@
-from tkinter import *
+# The code for changing pages was derived from: http://stackoverflow.com/questions/7546050/switch-between-two-frames-in-tkinter
+# License: http://creativecommons.org/licenses/by-sa/3.0/	
+
+import matplotlib
+matplotlib.use("TkAgg")
+from matplotlib.backends.backend_tkagg import FigureCanvasTkAgg, NavigationToolbar2TkAgg
 from matplotlib.figure import Figure
-from matplotlib.backends.backend_tkagg import (FigureCanvasTkAgg,NavigationToolbar2Tk)
+
+import tkinter as tk
+from tkinter import ttk
 
 
-# plot function is created for
-# plotting the graph in
-# tkinter window
-def plot():
+LARGE_FONT= ("Verdana", 12)
 
-	# the figure that will contain the plot
-	fig = Figure(figsize = (5, 5),
-				dpi = 100)
 
-	# list of squares
-	y = [i**2 for i in range(101)]
+class SeaofBTCapp(tk.Tk):
 
-	# adding the subplot
-	plot1 = fig.add_subplot(111)
+    def __init__(self, *args, **kwargs):
+        
+        tk.Tk.__init__(self, *args, **kwargs)
 
-	# plotting the graph
-	plot1.plot(y)
+        tk.Tk.iconbitmap(self, default="clienticon.ico")
+        tk.Tk.wm_title(self, "Sea of BTC client")
+        
+        
+        container = tk.Frame(self)
+        container.pack(side="top", fill="both", expand = True)
+        container.grid_rowconfigure(0, weight=1)
+        container.grid_columnconfigure(0, weight=1)
 
-	# creating the Tkinter canvas
-	# containing the Matplotlib figure
-	canvas = FigureCanvasTkAgg(fig,
-							master = window)
-	canvas.draw()
+        self.frames = {}
 
-	# placing the canvas on the Tkinter window
-	canvas.get_tk_widget().pack()
+        for F in (StartPage, PageOne, PageTwo, PageThree):
 
-	# creating the Matplotlib toolbar
-	toolbar = NavigationToolbar2Tk(canvas,
-								window)
-	toolbar.update()
+            frame = F(container, self)
 
-	# placing the toolbar on the Tkinter window
-	canvas.get_tk_widget().pack()
+            self.frames[F] = frame
 
-# the main Tkinter window
-window = Tk()
+            frame.grid(row=0, column=0, sticky="nsew")
 
-# setting the title
-window.title('Plotting in Tkinter')
+        self.show_frame(StartPage)
 
-# dimensions of the main window
-window.geometry("500x500")
+    def show_frame(self, cont):
 
-# button that displays the plot
-plot_button = Button(master = window,
-					command = plot,
-					height = 2,
-					width = 10,
-					text = "Plot")
+        frame = self.frames[cont]
+        frame.tkraise()
 
-# place the button
-# in main window
-plot_button.pack()
+        
+class StartPage(tk.Frame):
 
-# run the gui
-window.mainloop()
+    def __init__(self, parent, controller):
+        tk.Frame.__init__(self,parent)
+        label = tk.Label(self, text="Start Page", font=LARGE_FONT)
+        label.pack(pady=10,padx=10)
+
+        button = ttk.Button(self, text="Visit Page 1",
+                            command=lambda: controller.show_frame(PageOne))
+        button.pack()
+
+        button2 = ttk.Button(self, text="Visit Page 2",
+                            command=lambda: controller.show_frame(PageTwo))
+        button2.pack()
+
+        button3 = ttk.Button(self, text="Graph Page",
+                            command=lambda: controller.show_frame(PageThree))
+        button3.pack()
+
+
+class PageOne(tk.Frame):
+
+    def __init__(self, parent, controller):
+        tk.Frame.__init__(self, parent)
+        label = tk.Label(self, text="Page One!!!", font=LARGE_FONT)
+        label.pack(pady=10,padx=10)
+
+        button1 = ttk.Button(self, text="Back to Home",
+                            command=lambda: controller.show_frame(StartPage))
+        button1.pack()
+
+        button2 = ttk.Button(self, text="Page Two",
+                            command=lambda: controller.show_frame(PageTwo))
+        button2.pack()
+
+
+class PageTwo(tk.Frame):
+
+    def __init__(self, parent, controller):
+        tk.Frame.__init__(self, parent)
+        label = tk.Label(self, text="Page Two!!!", font=LARGE_FONT)
+        label.pack(pady=10,padx=10)
+
+        button1 = ttk.Button(self, text="Back to Home",
+                            command=lambda: controller.show_frame(StartPage))
+        button1.pack()
+
+        button2 = ttk.Button(self, text="Page One",
+                            command=lambda: controller.show_frame(PageOne))
+        button2.pack()
+
+
+class PageThree(tk.Frame):
+
+    def __init__(self, parent, controller):
+        tk.Frame.__init__(self, parent)
+        label = tk.Label(self, text="Graph Page!", font=LARGE_FONT)
+        label.pack(pady=10,padx=10)
+
+        button1 = ttk.Button(self, text="Back to Home",
+                            command=lambda: controller.show_frame(StartPage))
+        button1.pack()
+
+        f = Figure(figsize=(5,5), dpi=100)
+        a = f.add_subplot(111)
+        a.plot([1,2,3,4,5,6,7,8],[5,6,1,3,8,9,3,5])
+
+        
+
+        canvas = FigureCanvasTkAgg(f, self)
+        canvas.show()
+        canvas.get_tk_widget().pack(side=tk.BOTTOM, fill=tk.BOTH, expand=True)
+
+        toolbar = NavigationToolbar2TkAgg(canvas, self)
+        toolbar.update()
+        canvas._tkcanvas.pack(side=tk.TOP, fill=tk.BOTH, expand=True)
+
+        
+
+app = SeaofBTCapp()
+app.mainloop()
